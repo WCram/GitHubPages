ff7f2df4560da51af77896ea13e18d282dd7cbf5
WCram <39754629+WCram@users.noreply.github.com>
2021-10-11 21:25:44 -0700

    Added Javascript

---
+++ js/js.js
@@ -0,0 +1,21 @@
+window.onload = function() {
+	let str = "Hello Josh. Welcome to my website!";
+	printLine(str);
+
+	
+}
+
+function sleep(ms) {
+	return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function printLine(toDisplay = "Test") {
+	let display = document.getElementById("display");
+		
+	for(let i = 0; i < toDisplay.length; i++ ) {
+		await sleep(75);
+			display.innerHTML += toDisplay.charAt(i);
+	}
+	display.innerHTML += "</br>";
+	await sleep(1000);
+}
